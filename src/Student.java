e48e677d65b45d5d2f7da72c747776433c45b082
PleitesGC-12 <primezero04@gmail.com>
2025-09-08 13:21:57 -0600

    feat: add enrollment date to student (BUGGY)

--- src/Student.java
+++ src/Student.java
@@ -1,23 +1,25 @@
-public class Student implements java.io.Serializable {
+import java.util.Date;
 
+public class Student implements java.io.Serializable {
     private String name;
     private int id;
+
+    // 🔥 NEW: This field is NOT serializable and has no default value
+    private Date enrollmentDate;  // Problem: no default, breaks deserialization!
 
     public Student(String name, int id) {
         this.name = name;
         this.id = id;
+        this.enrollmentDate = new Date(); // set at creation
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
+    // Getters...
+    public String getName() { return name; }
+    public int getId() { return id; }
+    public Date getEnrollmentDate() { return enrollmentDate; }
 
     @Override
     public String toString() {
-        return "ID: " + id + " , Name: " + name;
+        return "ID: " + id + ", Name: " + name + ", Enrolled: " + enrollmentDate;
     }
 }
