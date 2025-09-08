a2d80cc2a1fc49cd66c467985d8de8d9875d9526
PleitesGC-12 <primezero04@gmail.com>
2025-09-08 13:33:38 -0600

    Revert "feat: add enrollment date to student (BUGGY)"
    This reverts commit e48e677d65b45d5d2f7da72c747776433c45b082.

--- src/Student.java
+++ src/Student.java
@@ -1,25 +1,23 @@
-import java.util.Date;
-
 public class Student implements java.io.Serializable {
+
     private String name;
     private int id;
-
-    // 🔥 NEW: This field is NOT serializable and has no default value
-    private Date enrollmentDate;  // Problem: no default, breaks deserialization!
 
     public Student(String name, int id) {
         this.name = name;
         this.id = id;
-        this.enrollmentDate = new Date(); // set at creation
     }
 
-    // Getters...
-    public String getName() { return name; }
-    public int getId() { return id; }
-    public Date getEnrollmentDate() { return enrollmentDate; }
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
 
     @Override
     public String toString() {
-        return "ID: " + id + ", Name: " + name + ", Enrolled: " + enrollmentDate;
+        return "ID: " + id + " , Name: " + name;
     }
 }
