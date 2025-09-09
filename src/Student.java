ae9417ddd0a4d90a1682b29a7b8f8d6b76d55052
PleitesGC-12 <primezero04@gmail.com>
2025-09-09 08:33:05 -0600

    feat: Add grade tracking to students

--- src/Student.java
+++ src/Student.java
@@ -1,11 +1,17 @@
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Student implements java.io.Serializable {
 
     private String name;
     private int id;
+    private Map <String, Integer> grades; // New field
 
     public Student(String name, int id) {
         this.name = name;
         this.id = id;
+        this.grades = new HashMap<>(); // Initialize
     }
 
     public String getName() {
@@ -14,6 +20,20 @@ public class Student implements java.io.Serializable {
 
     public int getId() {
         return id;
+    }
+
+    public Map <String, Integer> getGrades() {
+        return grades;
+    }
+
+    // Method to add a grade
+    public void addGrade(String subject, int grade) {
+        if (grade < 0 || grade > 100) {
+            System.out.println("Invalid grade: " + grade);
+            return;
+        }
+        grades.put(subject,grade);
+        System.out.println("Grade added: " + subject + " -> " + grade);
     }
 
     @Override
